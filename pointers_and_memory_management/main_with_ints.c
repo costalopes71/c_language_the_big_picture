32004b24fa26bea9103c6517007057692d38a3aa
Joao Lopes <joao.lopes+joao.lopes@elo7.com>
2021-09-13 23:11:17 -0300

    playing with memory 2

---
+++ pointers_and_memory_management/main_with_ints.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    
+    int nums1[5] = {1, 2, 3, 4, 5};
+
+    int *nums2 = nums1;
+
+    printf("nums1 --> %p\n", nums1);
+    printf("nums2 --> %p\n\n", nums2);
+
+    nums2 += 2;
+
+    printf("nums1 --> %p\n", nums1);
+    printf("nums2 --> %p\n\n", nums2);
+
+    *nums2 = 100;
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ", nums1[i]);
+    }
+    
+    printf("\n");
+
+    return 0;
+}
