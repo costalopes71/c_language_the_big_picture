1a1b08d66df96530b2da80753b11f7bd85019b3e
Joao Lopes <joao.lopes+joao.lopes@elo7.com>
2021-09-13 23:01:09 -0300

    playing with memory management 1

---
+++ pointers_and_memory_management/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    
+    char name1[] = "Brice";
+
+    printf("%s\n\n", name1);
+
+    char *name2 = name1;
+
+    printf("name1 --> %p\n", name1);
+    printf("name2 --> %p\n\n", name2);
+
+    name2 += 2;
+
+    printf("name1 --> %p\n", name1);
+    printf("name2 --> %p\n\n", name2);
+
+    *name2 = 'y';
+
+    printf("%s\n", name1);
+
+    return 0;
+}
